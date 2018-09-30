ea7b8442b4917db16e1697bb5dac7982eb922b47
Jue <me@jue.yt>
2018-09-30 17:41:47 +0200

    Bug fix: We must use fstat instead of stat

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,5 +1,5 @@
 module.exports.open = jest.fn((path, flags, cb) => cb(null, 1));
 module.exports.write = jest.fn((fd, buffer, offset, length, position, cb) => cb(null, length, buffer));
 module.exports.read = jest.fn((fd, buffer, offset, length, position, cb) => cb(null, length, buffer));
-module.exports.stat = jest.fn((fd, cb) => cb(null, {}));
+module.exports.fstat = jest.fn((fd, cb) => cb(null, {}));
 module.exports.close = jest.fn((fd, cb) => cb(null));
