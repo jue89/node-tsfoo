52d2e9f0fa5854f8198460a597f74313dbe2249b
Jue <me@jue.yt>
2018-09-30 16:35:24 +0200

    Separated mocks from tests

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,3 +1,3 @@
 module.exports.open = jest.fn((path, flags, cb) => cb(null, 1));
-module.exports.write = jest.fn((fd, buffer, offset, len, pos, cb) => cb(null, len));
+module.exports.write = jest.fn((fd, buffer, cb) => cb(null, buffer.length));
 module.exports.close = jest.fn((fd, cb) => cb(null));
