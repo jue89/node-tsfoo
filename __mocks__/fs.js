7e7108832f672fc936c8fd4a581a4b774a05037e
Jue <me@jue.yt>
2018-09-30 16:55:52 +0200

    series: Refactored write calls

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,3 +1,4 @@
 module.exports.open = jest.fn((path, flags, cb) => cb(null, 1));
-module.exports.write = jest.fn((fd, buffer, cb) => cb(null, buffer.length));
+module.exports.write = jest.fn((fd, buffer, offset, length, position, cb) => cb(null, length, buffer));
+module.exports.read = jest.fn((fd, buffer, offset, length, position, cb) => cb(null, length, buffer));
 module.exports.close = jest.fn((fd, cb) => cb(null));
