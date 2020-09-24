26231ae92508fde946493970ba79096e485c0419
Juergen Fitschen <me@jue.yt>
2020-09-24 17:50:50 +0200

    fsbackend: fixed FSWatcher interface

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -2,7 +2,7 @@ module.exports.constants = {F_OK: true};
 module.exports.open = jest.fn((path, flags, cb) => cb(null, 1));
 module.exports.stat = jest.fn((path, cb) => cb(null, {size: 0}));
 module.exports.watchFile = jest.fn();
-module.exports.watch = jest.fn(() => ({ unwatch: jest.fn() }));
+module.exports.watch = jest.fn(() => ({ close: jest.fn() }));
 module.exports.unwatchFile = jest.fn();
 module.exports.access = jest.fn((f, m, cb) => cb(null));
 module.exports.read = jest.fn((f, b, o, l, p, cb) => cb(null, b.length, b));
